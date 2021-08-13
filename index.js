e4915859ed3cb25af2f4dda6a06146e75d73f8fa
yungbentai <74857832+yungbentai@users.noreply.github.com>
2021-08-14 03:37:14 +0800

    phresh tech

--- index.js
+++ index.js
@@ -1,180 +1,117 @@
+const nums=[7,8,9,4,5,6,1,2,3,0];
+const ops=['÷','x','-','+']
+
 class App extends React.Component {
     state ={
-        currentNumber: '0',
-        calc: undefined,
+        lastPressed: undefined,
+        calc: '0',
         operation: undefined,
-        sequence: undefined
+        div: "/",
+        mul: "*",
+        neg: "-"
     }
 
     handleClick = (event) => {
-        const {currentNumber, calc, operation, sequence} = this.state;
+        const {calc, lastPressed, div, mul, neg} = this.state;
         const {innerText} = event.target;
-
-        if(!Number.isNaN(Number(innerText))) {
-
-            if(currentNumber === '0') {
-                this.setState({
-                    currentNumber:innerText
-                });
-            } 
-
-            else if(currentNumber === calc) {
-                this.setState({
-                    currentNumber:innerText,
-                    calc:undefined,
-                    sequence: undefined
-                });
-
-            } 
-            
-            else if(currentNumber === '-') {
-
-                this.setState({
-                    currentNumber: "0",
-                    calc: currentNumber + innerText,
-                    operation: undefined
-                })
-
-            } else {
-                this.setState({
-                    currentNumber: currentNumber + innerText
-                });
-            }
-
-            return;
-        }
 
         switch(innerText) {
             case 'AC': {
                 this.setState({
-                    currentNumber: '0',
-                    calc: undefined,
-                    operation: undefined,
-                    sequence: undefined,
-                    store: undefined
+                    calc: '0',
                 });
                 break;
              }
-
-            case '.': {
-                if(!currentNumber.includes('.')) {
-                    this.setState({
-                        currentNumber: currentNumber + innerText
-                    });
-                 };
-                 break;
+            case '=': {
+                const evaluated = eval(calc);
+                this.setState({
+                    calc: evaluated
+                });
+                break;
             }
-
-            default: {
-                if(!operation) { 
-                    if(innerText === 'x') {
-                        const bruh = '*'
-                        this.setState({
-                            operation: bruh,
-                            calc: currentNumber,
-                            currentNumber: '0'
-                        });
-                    } 
-                    else if (innerText === '÷') {
-                        const pog = '/'
-                        this.setState({
-                            operation: pog,
-                            calc: currentNumber,
-                            currentNumber: '0'
-                        });
-                    } 
-                    else if(innerText === '=') {
-                        
-                        this.setState({
-                            currentNumber: currentNumber,
-                            calc: undefined,
-                            sequence:undefined
-                        });
-
-                    } else {
-                        this.setState({
-                            operation: innerText,
-                            calc: currentNumber,
-                            currentNumber: '0'
-                        });
-                    }
-                } else if (innerText === "=") {
-                    const evaluated = eval(`${calc} ${operation} ${currentNumber}`);
-                    this.setState({
-                        operation: undefined,
-                        calc: evaluated,
-                        currentNumber: evaluated,
-                    });
-                } else {
-                    const evaluated = eval(`${calc} ${operation} ${currentNumber}`);
-                    
-                    if(innerText === '-') {
-                        this.setState({
-                                operation: innerText,
-                                calc: evaluated,
-                                currentNumber: '0',
-                        })
-
-                    } else {
+            case '.':{
+                const splitted = calc.split(/[\+\-\x\÷]/);
+                const last = splitted.slice(-1)[0];
                 
-                        if(innerText === 'x') {
-                            const bruh = '*'
-                            this.setState({
-                                operation: bruh,
-                                calc: evaluated,
-                                currentNumber: '0'
-                            });
-                        } 
-                        else if (innerText === '÷') {
-                            const pog = '/'
-                            this.setState({
-                                operation: pog,
-                                calc: evaluated,
-                                currentNumber: '0',
-                            });
-                        } else {
-                            this.setState({
-                                operation: innerText,
-                                calc: evaluated,
-                                currentNumber: '0',
-                            });
-                        }   
-                    } 
+                if(!last.includes('.')){
+                  this.setState({
+                    calc:calc+'.'
+                  })
                 }
+                break;
+            }
+            default: {
+                
+                let e = undefined;
+
+                if(ops.includes(innerText)) {
+                    if(ops.includes(lastPressed) && innerText !== '-') {
+
+                        const lastNumberIdx = calc.split('').reverse().findIndex(char=>char!==' ' && nums.includes(+char));
+
+                        e = calc.slice(0,calc.length - lastNumberIdx) + `${innerText}`; 
+                   
+                    } else {
+                       if (innerText === "÷") {
+
+                            e =  `${calc}${div}`
+
+                       }
+                       
+                       else if (innerText === "x") {
+
+                            e= `${calc}${mul}`
+                        
+                       } else {
+                        e =  `${calc}${innerText}`
+                       }
+                    }
+                } else {
+                    e = (calc === '0') ? innerText : (calc + innerText);
+                }
+
+                this.setState({
+                    calc: e 
+                });
             }
         }
+
+        this.setState({
+            lastPressed: innerText
+          })
+
     }
 
     render() {
 
-        const {operation, currentNumber, calc} = this.state;
+        const {calc} = this.state;
         return(
         <div id="calc-container" className="d-flex align-items-center justify-content-center">
         <div className="layout">
-        {/*<p style={{position:'absolute', top:0}}>{JSON.stringify(this.state,null,2)}</p>*/}
+        {/* <p style={{position:'absolute', top:0}}>{JSON.stringify(this.state,null,2)}</p> */}
             <div id="screen">
                 <div id="logo" className="d-flex align-item-left">
                     <i className="fas fa-calculator"></i>Epic Calculator
                 </div>
-                <div id="display"><small>{calc} {operation}</small>{currentNumber}</div>    
+                <div id="display">{calc}</div>    
             </div>
 
             <div className="col-12 button-wrapper">
                 <button id="clear" onClick={this.handleClick}>AC</button>
-                <button id="divide" className="operations" key={"÷"} onClick={this.handleClick}>÷</button>
-                <button id="seven" className="integers" key={7} onClick={this.handleClick}>7</button>
-                <button id="eight" className="integers" key={8} onClick={this.handleClick}>8</button>
-                <button id="nine" className="integers" key={9} onClick={this.handleClick}>9</button>
-                <button id="multiply"className="operations" key={"×"} onClick={this.handleClick}>x</button>
-                <button id="four" className="integers" key={4} onClick={this.handleClick}>4</button>
-                <button id="five" className="integers" key={5} onClick={this.handleClick}>5</button>
-                <button id="six" className="integers" key={6} onClick={this.handleClick}>6</button> 
+                <button id="divide" className="operations" onClick={this.handleClick}>÷</button>
+                <button id="seven" className="integers" onClick={this.handleClick}>7</button>
+                <button id="eight" className="integers" onClick={this.handleClick}>8</button>
+                <button id="nine" className="integers" onClick={this.handleClick}>9</button>
+                <button id="multiply"className="operations" onClick={this.handleClick}>x</button>
+                <button id="four" className="integers" onClick={this.handleClick}>4</button>
+                <button id="five" className="integers" onClick={this.handleClick}>5</button>
+                <button id="six" className="integers" onClick={this.handleClick}>6</button> 
                 <button id="subtract" className="operations" key={"-"} onClick={this.handleClick}>-</button>
-                <button id="one" className="integers" key={1} onClick={this.handleClick}>1</button>
-                <button id="two" className="integers" key={2} onClick={this.handleClick}>2</button>
-                <button id="three" className="integers" key={3} onClick={this.handleClick}>3</button>
-                <button id="add"className="operations" key={"+"} onClick={this.handleClick}>+</button>
-                <button id="zero" className="integers" key={0} onClick={this.handleClick}>0</button>
+                <button id="one" className="integers" onClick={this.handleClick}>1</button>
+                <button id="two" className="integers" onClick={this.handleClick}>2</button>
+                <button id="three" className="integers" onClick={this.handleClick}>3</button>
+                <button id="add"className="operations" onClick={this.handleClick}>+</button>
+                <button id="zero" className="integers" onClick={this.handleClick}>0</button>
                 <button id="decimal" onClick={this.handleClick}>.</button>
                 <button id="equals" key={"="} onClick={this.handleClick}>=</button>
             </div>
